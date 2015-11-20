929764e58a749ecffcd08c9f290c8c0ca9d9bb74
Travis Parks <jehugaleahsa@gmail.com>
2015-11-20 17:42:18 -0500

    Add strong-name to assemblies

--- Deployment/publish-mustache-sharp.bat
+++ Deployment/publish-mustache-sharp.bat
@@ -1,4 +1,4 @@
-msbuild ../mustache-sharp.sln /p:Configuration=Release
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" ../mustache-sharp.sln /p:Configuration=Release
 nuget pack ../mustache-sharp/mustache-sharp.csproj -Properties Configuration=Release
 nuget push *.nupkg
 del *.nupkg
