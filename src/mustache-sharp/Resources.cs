545f8749b8d9ddbb0ecb66e5f7f0075e3e8f46e0
hamedfathi <hamedfathi@outlook.com>
2016-12-20 10:32:39 +0330

    .net core branch ;)

---
+++ src/mustache-sharp/Resources.cs
@@ -0,0 +1,44 @@
+﻿namespace Mustache
+{
+    internal static class Resources
+    { /// <summary>
+      ///   Looks up a localized string similar to An attempt was made to define a parameter with a null or an invalid identifier..
+      /// </summary>
+        internal static string BlankParameterName => "BlankParameterName";
+
+        /// <summary>
+        ///   Looks up a localized string similar to An attempt was made to define a tag with a null or an invalid identifier..
+        /// </summary>
+        internal static string BlankTagName => "BlankTagName";
+
+        /// <summary>
+        ///   Looks up a localized string similar to A parameter with the same name already exists within the tag..
+        /// </summary>
+        internal static string DuplicateParameter => "DuplicateParameter";
+
+        /// <summary>
+        ///   Looks up a localized string similar to The {0} tag has already been registered..
+        /// </summary>
+        internal static string DuplicateTagDefinition => "DuplicateTagDefinition";
+
+        /// <summary>
+        ///   Looks up a localized string similar to The key {0} could not be found..
+        /// </summary>
+        internal static string KeyNotFound => "KeyNotFound";
+
+        /// <summary>
+        ///   Looks up a localized string similar to Expected a matching {0} tag but none was found..
+        /// </summary>
+        internal static string MissingClosingTag => "MissingClosingTag";
+
+        /// <summary>
+        ///   Looks up a localized string similar to Encountered an unknown tag: {0}. It was either not registered or exists in a different context..
+        /// </summary>
+        internal static string UnknownTag => "UnknownTag";
+
+        /// <summary>
+        ///   Looks up a localized string similar to The wrong number of arguments were passed to an {0} tag..
+        /// </summary>
+        internal static string WrongNumberOfArguments => "WrongNumberOfArguments";
+    }
+}
